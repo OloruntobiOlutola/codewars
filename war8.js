fb80dfed3ead44b6d90dd7c41f701037468e3e34
Olutola Paul <tobaforjesus@gmail.com>
2022-07-31 21:04:13 +0100

    Another war

---
+++ war8.js
@@ -0,0 +1,29 @@
+/*
+ * Author: Olutola Oloruntobi Paul
+ * Date: 31/07/2022
+ */
+
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+// Examples
+// " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  =>  "#HelloWorld"
+// ""                                        =>  false
+
+function generateHashtag(str) {
+  str = str.split(" ");
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].replace(str[i].charAt(0), str[i].charAt(0).toUpperCase());
+  }
+  str = str.join("");
+
+  if (str.length > 139 || str.length === 0) return false;
+  return `#${str}`;
+}
